5b81b2616cd48667f325e90ae85cba10731e1420
AlyMarwan <amarwan43@gmail.com>
2024-08-11 19:31:50 +0300

    uploading to github

--- dynamicSite.js
+++ dynamicSite.js
@@ -1,6 +1,17 @@
-let i = 1000;
-var x = setInterval(function(){
-    var now = i--;
-    document.getElementById("demo").innerHTML = now;
-},1000);
-    
+const input = document.getElementById("timer");
+const output = document.getElementById("demo");
+
+
+function startTimer(){
+    let inputValue = parseInt(input.value);
+    let x = setInterval(function(){
+        let m = Math.floor(inputValue/60);
+        let s = inputValue % 60;   
+        output.textContent = m + " minutes and "+ s + " seconds.";
+        if(inputValue == 0){
+            output.textContent = "Timer done"; 
+            clearInterval(x);
+        }
+        inputValue--;
+    },1000)}
+
